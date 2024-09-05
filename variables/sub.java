e6957e6302a2d2af025c96961250d9ccfadcf608
acer <anjaligawande272001@gmail.com>
2024-09-05 12:15:27 +0530

    commit

--- variables/sub.java
+++ variables/sub.java
@@ -4,6 +4,7 @@ public class sub {
     public static void main(String[] args) {
         int a=12;
         int b=6;
-        System.out.println(a-b);
+        int substraction =a-b;
+        System.out.println("sub is" +substraction);
     }
 }
